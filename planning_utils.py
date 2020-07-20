b28f22ff42d5f002ecb836d3f6722e21406a9579
ashutosh.badave <badave007@gmail.com>
2020-07-20 14:03:49 +0200

    CU: Diagonal motions included. Added diagonal motions with cost of sqrt(2) in action and valid action as well.

--- planning_utils.py
+++ planning_utils.py
@@ -56,13 +56,18 @@ class Action(Enum):
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
 
+    NORTH_WEST = (-1,-1,np.sqrt(2))
+    WEST_SOUTH = (1,-1,np.sqrt(2))
+    SOUTH_EAST = (1,1,np.sqrt(2))
+    EAST_NORTH = (-1,1,np.sqrt(2))
+
     @property
     def cost(self):
         return self.value[2]
 
     @property
     def delta(self):
-        return (self.value[0], self.value[1])
+        return self.value[0], self.value[1]
 
 
 def valid_actions(grid, current_node):
@@ -84,6 +89,15 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
+
+    if x - 1 < 0 or y - 1 < 0 or grid[x - 1, y - 1] == 1:
+        valid_actions.remove(Action.NORTH_WEST)
+    if x + 1 > n or y - 1 < 0 or grid[x + 1, y - 1] == 1:
+        valid_actions.remove(Action.WEST_SOUTH)
+    if x + 1 > n or y + 1 > m or grid[x + 1, y + 1] == 1:
+        valid_actions.remove(Action.SOUTH_EAST)
+    if x - 1 < 0 or y + 1 > m or grid[x - 1, y + 1] == 1:
+        valid_actions.remove(Action.EAST_NORTH)
 
     return valid_actions
 
